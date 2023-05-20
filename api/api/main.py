8fb4b25ece32748096df2a38b5808d3471d0b0f0
Takumi Abe <ta93abe@ta93abe.com>
2023-05-20 20:53:38 +0900

    jinja parser api

--- api/api/main.py
+++ api/api/main.py
@@ -1,5 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+from jinja2 import Template
 
 app = FastAPI()
 
@@ -10,3 +12,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+class TemplateRequest(BaseModel):
+    template: str
+    variables: dict
+
+@app.post("/parse")
+async def parse_template(request: TemplateRequest):
+    template = Template(request.template)
+    result = template.render(**request.variables)
+    return {"result": result}
